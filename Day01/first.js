6863d5c5d9da883901131404caaff4efaba1b470
deepak108-sudo <deepakkr977140@gmail.com>
2025-10-24 02:48:13 +0530

    first commit

---
+++ Day01/first.js
@@ -0,0 +1,19 @@
+console.log("Hello coder army");
+
+let num=10;
+console.log(num);
+
+let name="Deepak";
+console.log(name);
+
+const n1=20;
+//n1=30;
+num=55;
+
+console.log(n1);
+console.log(num);
+
+//var not being use due to some reason
+var x=10;
+x=65;
+console.log(x);
