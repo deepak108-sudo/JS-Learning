9c9b8af4857e2bcee1ea2b11dac2d94358a82278
deepak108-sudo <deepakkr977140@gmail.com>
2025-10-25 18:45:39 +0530

    data types in js

---
+++ Day2/first.js
@@ -0,0 +1,31 @@
+let num="10";
+console.log(num);
+
+console.log(typeof num);
+console.log("num");
+
+let min=10;
+console.log(min);
+
+let comment='Hello Doston';
+console.log(comment);
+
+//boolean
+let statement=false;
+console.log(typeof statement);
+
+//undefined
+let account;
+console.log(account);
+console.log(typeof account);
+
+//null
+let bal=null;
+console.log(typeof bal);
+
+//bigint
+let a=6456445645645645643655454n;
+console.log(a);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
