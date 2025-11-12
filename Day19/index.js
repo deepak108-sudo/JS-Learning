b8bee53e5567a6fe03fa4e97733f5e8b0d20b7de
deepak108-sudo <deepakkr977140@gmail.com>
2025-11-13 04:06:12 +0530

    added dom

---
+++ Day19/index.js
@@ -0,0 +1,9 @@
+document.getElementById('second').innerHTML='No topic covered';
+document.getElementById('first').innerHTML='Syllabus'
+
+const obj=document.getElementById("first");
+const obj1=document.getElementsByClassName("header1")
+obj1[1].style.background("pink")
+
+const obj2=document.getElementById("first")
+obj2.style.fontSize="60px"
